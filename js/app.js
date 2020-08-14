23125d7b88edb135a76a77f104c6fb71ed2d77ba
mamadou-aly-sy <mo.aly.sy@gmail.com>
2020-08-14 14:09:25 +0000

    modification du composant footer

--- js/app.js
+++ js/app.js
@@ -1,66 +1,31 @@
-const styles = (hours, minutes, seconds) => ({
-  hours: {
-    height: 5,
-    transform: 'rotateZ(' + ((hours * 30) + (minutes * 0.5)) + 'deg)',
-    width: 70
-  },
-  minutes: {
-    height: 2.5,
-    transform: 'rotateZ(' + ((minutes * 6) + (seconds * 0.1)) + 'deg)',
-    width: 85
-  },
-  seconds: {
-    height: 1,
-    transform: 'rotateZ(' + seconds * 6 + 'deg)',
-    width: 100
+class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
   }
-})
-const hourDeg = (360 / 12) + ((360 / 12) / 60)
-class App extends React.Component {
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.runTimer(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  runTimer() {
+    this.setState({
+      date: new Date(),
+    });
+  }
+
   render() {
-    const date = new Date()
     return (
-      <main>
-        <AnalogClock date={date} />
-        <NumericClock date={date} />
-        <Footer />
-      </main>
-    )
+      <footer>
+        <p>{this.state.date.toLocaleTimeString()}</p>
+        <small>Elhadj@reactjs.org</small>
+      </footer>
+    );
   }
 }
 
-const Footer = () => {
-  return (
-    <footer>
-      <p>Elhadj@reactjs.org</p>
-    </footer>
-  )
-}
-
-const NumericClock = (props) => {
-  return (
-    <figcaption>
-      {props.date.toLocaleTimeString()}
-    </figcaption>
-  )
-}
-
-const AnalogClock = (props) => {
-  let hours = props.date.getHours()
-  if (hours > 12) { hours = hours - 12 }
-  const minutes = props.date.getMinutes()
-  const seconds = props.date.getSeconds()
-  return (
-    <figure>
-      <span style={styles(hours, minutes, seconds).hours} />
-      <span style={styles(hours, minutes, seconds).minutes} />
-      <span style={styles(hours, minutes, seconds).seconds} />
-    </figure>
-  )
-}
-
-function tick() {
-  ReactDOM.render(<App date={new Date()} />, document.getElementById('root'));
-}
-
-setInterval(tick, 1000);
+ReactDOM.render(<Footer />, document.getElementById("root"));
